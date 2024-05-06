2bfe0e60def424b6fda1f4a6361a8aa0815eb8f1
Itohan <momoduitohan5@gmail.com>
2024-05-06 22:37:47 +0100

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,7 +22,7 @@ fi
 #}
 sudo mkdir -p "$data_folder" "$data_static" "$dara_releases" "$data_shared" "$data_test"
 sudo chown -R ubuntu:ubuntu "$data_folder"
-sudo chmod -R +x "$data_folder"
+sudo chmod -R ug+rwx "$data_folder"
 #check_file "$data_folder"
 #check_file "$data_static"
 #check_file "$data_releases"

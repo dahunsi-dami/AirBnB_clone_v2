4589f08016689607b56933c9c879f5f5f08ed774
Itohan <momoduitohan5@gmail.com>
2024-05-06 23:32:35 +0100

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,8 +21,8 @@ check_file() {
         fi
 }
 #sudo mkdir -p "$data_folder" "$data_static" "$dara_releases" "$data_shared" "$data_test"
-sudo chown -R ubuntu:ubuntu "$data_static"
-sudo chmod -R 755 "$data_static"
+sudo chown -R ubuntu:ubuntu "$data_folder"
+sudo chmod -R 755 "$data_folder"
 check_file "$data_folder"
 check_file "$data_static"
 check_file "$data_releases"

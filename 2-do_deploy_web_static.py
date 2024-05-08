81147940e55715b5f8535da7982e77b3c11c1bed
dahunsi-dami <gbemigadare@gmail.com>
2024-05-08 23:42:58 +0100

    Shorten edge case, do split twice & use accordingly.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,38 +23,37 @@ def do_deploy(archive_path):
 
     try:
         # Check if file at archive_path exists
-        if path.isfile(archive_path):
-            pass
-        else:
+        if not path.isfile(archive_path):
             return False
 
         # Upload archive to /tmp/ directory of web server
         put(archive_path, '/tmp/')
 
         # Create folder that'll store uncompressed archive.
-        filename = archive_path.split("/")[-1].split(".")[0]
-        run(f'sudo mkdir -p /data/web_static/releases/web_static_{filename}/')
+        fname = archive_path.split("/")[-1]
+        sname = fname.split(".")[0]
+        run(f'sudo mkdir -p /data/web_static/releases/web_static_{sname}/')
 
         # Uncompress archive to created folder
-        run(f'sudo tar -xzf /tmp/{filename}.tgz -C \
-        /data/web_static/releases/web_static_{filename}/')
+        run(f'sudo tar -xzf /tmp/{fname} -C \
+        /data/web_static/releases/web_static_{sname}/')
 
         # Delete the archive from the server
-        run(f'sudo rm /tmp/web_static_{filename}.tgz')
+        run(f'sudo rm /tmp/{fname}')
 
         # Move files from web_static to parent folder
-        run(f'sudo mv /data/web_static/releases/web_static_{filename}\
-        /web_static/* /data/web_static/releases/web_static_{filename}/')
+        run(f'sudo mv /data/web_static/releases/web_static_{sname}\
+        /web_static/* /data/web_static/releases/web_static_{sname}/')
 
         # Remove web_static folder
-        run(f'sudo rm -rf /data/web_static/releases/web_static_{filename}\
+        run(f'sudo rm -rf /data/web_static/releases/web_static_{sname}\
         /web_static/')
 
         # Delete symbolic link /data/web_static/current from web server
         run('sudo rm -rf /data/web_static/current')
 
         # Create new symbolic link /data/web_static/current on web server
-        run(f'sudo ln -s /data/web_static/releases/web_static_{filename}/ \
+        run(f'sudo ln -s /data/web_static/releases/web_static_{sname}/ \
             /data/web_static/current')
     except Exception as e:
         return False

a1f80f5b33dd2f7b295158baf0764295c5abfffc
dahunsi-dami <gbemigadare@gmail.com>
2024-05-08 22:16:18 +0100

    Fix pycodestyle adherence errors.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,11 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
+
 def do_pack():
     """
     do_pack generates .tgz archive w/ web_static content.
     """
-
 
     # Create `versions` folder if it doesn't exist already.
     local('sudo mkdir -p versions')

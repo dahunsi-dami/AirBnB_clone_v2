74493ebb5105ea2314428f52a256aff3bfbfc8ed
dahunsi-dami <gbemigadare@gmail.com>
2024-05-18 17:24:59 +0100

    Resolve pycodestyle issues.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays Hello HBNB!"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
